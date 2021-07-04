c80d4d74bf9c976e87433e4740611c182013c4ef
1190202328 <1259029328@qq.com>
2021-07-04 16:20:52 +0800

    v1.0
    可以将页面转换成为txt文档了

--- src/BlogAutomaticScoring.py
+++ src/BlogAutomaticScoring.py
@@ -11,13 +11,22 @@ class BlogAutomaticScoring:
 
     @staticmethod
     def get_text(url):
+        """
+        根据url地址获取页面内容
+        :param url: URL地址
+        :return: 页面文档
+        """
         req = requests.get(url=url, headers={'User-Agent': 'Baiduspider'})
         html = req.text
-        bf = BeautifulSoup(html)
-        head = bf.find_all("title", style="text-indent:33px;")
-        p = bf.find_all("p")
         text = ""
         pattern = "</p>|<p(.*)>|\\n"
-        for i in range(len(p)):
-            text += re.sub(pattern, "", p[i].text, count=0, flags=0)
+        bf = BeautifulSoup(html)
+        contents = bf.find_all("div", class_="blog-content-box")
+        for content in contents:
+            if content.class_ == ("blog-tags-box" or "bar-content"):
+                continue
+            if (content.tag == "p" or "h1" or "ul") and not content.text.isspace():
+                text += re.sub(pattern, "\n", content.text, count=0, flags=0) + "\n"
+        text = re.sub("[ \t]", "", text)
+        text = re.sub("\n+", "\n", text)
         return text
---
+++ test/BlogAutomaticScoringTest.py
@@ -0,0 +1,8 @@
+import unittest
+
+from src.BlogAutomaticScoring import BlogAutomaticScoring
+
+
+class BlogAutomaticScoringTest(unittest.Testcase):
+    def test_get_text(self):
+        txt = ""
